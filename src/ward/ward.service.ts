8f6bec13b62bdded0a17075442decd0e9b9af5fa
FangLy <thefangsni@gmail.com>
2024-12-06 02:35:39 +0700

    fix clear ward cache

--- src/ward/ward.service.ts
+++ src/ward/ward.service.ts
@@ -3,13 +3,15 @@ import { CreateWardDto } from './dto/create-ward.dto';
 import { UpdateWardDto } from './dto/update-ward.dto';
 import { PrismaService } from '../prisma/prisma.service';
 import { dateFormat } from '../common/utils';
+import { RedisService } from '../redis/redis.service';
 
 @Injectable()
 export class WardService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService, private readonly redis: RedisService) {}
   async create(createWardDto: CreateWardDto) {
     createWardDto.createAt = dateFormat(new Date());
     createWardDto.updateAt = dateFormat(new Date());
+    await this.redis.del("hospital");
     return this.prisma.wards.create({ data: createWardDto, include: { hospital: true } });
   }
 
@@ -23,10 +25,12 @@ export class WardService {
 
   async update(id: string, updateWardDto: UpdateWardDto) {
     updateWardDto.updateAt = dateFormat(new Date());
+    await this.redis.del("hospital");
     return this.prisma.wards.update({ where: { id }, data: updateWardDto });
   }
 
   async remove(id: string) {
+    await this.redis.del("hospital");
     return this.prisma.wards.delete({ where: { id } });
   }
 }

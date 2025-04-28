544157f420ff41ac81342ce36c40b9a8d02ec06d
FangLy <thefangsni@gmail.com>
2025-04-28 15:19:16 +0700

    feat: update Docker image version to 1.1.3 and add RabbitMQ config to deployment

--- src/rabbitmq/rabbitmq.module.ts
+++ src/rabbitmq/rabbitmq.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { RabbitmqService } from './rabbitmq.service';
 
+@Global()
 @Module({
   imports: [ 
     ClientsModule.register([
